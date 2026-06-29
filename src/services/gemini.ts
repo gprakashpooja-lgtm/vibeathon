804fe1c1304930f8b541dd4e45560c5c9693a3a9
Aishwarya <gprakashpooja@gmail.com>
2026-06-29 22:51:55 +0530

    Updated gemini.ts

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -8,7 +8,7 @@ export async function generateRoomDesign(
   const base64 = image.split(",")[1];
 
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-preview-image-generation?key=${API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-preview-image-generation?key=${AQ.Ab8RN6Jt6Dy2tgpzzAWbZjdN-1kHSKqg9Jc7Wc4_0e4WNS5drw}`,
     {
       method: "POST",
       headers: {
